f71c5c74d0c5bb08f164225cf58600fcc169279a
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-20 11:55:48 +0700

    //

--- OrderApp/App_Start/RouteConfig.cs
+++ OrderApp/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace OrderApp
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapMvcAttributeRoutes();// đăng ký router vào controller
 
             routes.MapRoute(
                 name: "Default",
