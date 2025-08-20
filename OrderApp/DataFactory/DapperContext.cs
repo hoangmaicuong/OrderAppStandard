c07024927cf0b03817378b6561f1a018350dd8d2
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-20 11:59:42 +0700

    //

---
+++ OrderApp/DataFactory/DapperContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace OrderApp.DataFactory
+{
+    public class DapperContext
+    {
+        public DapperContext() { }
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString);
+        }
+    }
+}
