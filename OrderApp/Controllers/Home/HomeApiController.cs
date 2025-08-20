b0357a8a0a8f7eb780dd8d5d816d7c32d6552def
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-20 11:42:43 +0700

    //

---
+++ OrderApp/Controllers/Home/HomeApiController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+namespace OrderApp.Controllers.Home
+{
+    public class HomeApiController : ApiController
+    {
+    }
+}
