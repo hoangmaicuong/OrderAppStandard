c3a2e0b3f2647d532cf43bcfb5286f27d877799b
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-20 11:51:10 +0700

    sample

---
+++ OrderApp/Areas/Admin/Controllers/AdminSample/AdminSampleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+
+namespace OrderApp.Areas.Admin.Controllers._1Sample
+{
+    //[Authorize(Roles = "admin")]
+    public class AdminSampleController : Controller
+    {
+        // GET: Admin/AdminSample
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail(int id = 0)
+        {
+            ViewBag.id = id;
+            return View();
+        }
+        public ActionResult SinglePage()
+        {
+            return View();
+        }
+        public ActionResult SinglePageList()
+        {
+            return View();
+        }
+    }
+}
