d13014a1eb7306bb5d895658778280c5bc39122b
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-21 14:38:17 +0700

    add template

---
+++ OrderApp/Areas/Admin/Controllers/AdminHome/AdminHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderApp.Areas.Admin.Controllers.AdminHome
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: Admin/AdminHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
