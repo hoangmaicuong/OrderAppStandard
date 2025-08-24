533108e15e7ccbb1cb9f52004721d856c86794ca
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-24 16:49:32 +0700

    //

--- OrderApp/assets/js/admin.js
+++ OrderApp/assets/js/admin.js
@@ -77,112 +77,6 @@ function getMoney() {
 function vnd(price) {
     return price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' });
 }
-// Phân trang 
-//let perPage = 12;
-//let currentPage = 1;
-//let totalPage = 0;
-//let perProducts = [];
-
-//function displayList(productAll, perPage, currentPage) {
-//    let start = (currentPage - 1) * perPage;
-//    let end = (currentPage - 1) * perPage + perPage;
-//    let productShow = productAll.slice(start, end);
-//    showProductArr(productShow);
-//}
-
-//function setupPagination(productAll, perPage) {
-//    document.querySelector('.page-nav-list').innerHTML = '';
-//    let page_count = Math.ceil(productAll.length / perPage);
-//    for (let i = 1; i <= page_count; i++) {
-//        let li = paginationChange(i, productAll, currentPage);
-//        document.querySelector('.page-nav-list').appendChild(li);
-//    }
-//}
-
-//function paginationChange(page, productAll, currentPage) {
-//    let node = document.createElement(`li`);
-//    node.classList.add('page-nav-item');
-//    node.innerHTML = `<a href="#">${page}</a>`;
-//    if (currentPage == page) node.classList.add('active');
-//    node.addEventListener('click', function () {
-//        currentPage = page;
-//        displayList(productAll, perPage, currentPage);
-//        let t = document.querySelectorAll('.page-nav-item.active');
-//        for (let i = 0; i < t.length; i++) {
-//            t[i].classList.remove('active');
-//        }
-//        node.classList.add('active');
-//    })
-//    return node;
-//}
-
-//// Hiển thị danh sách sản phẩm 
-//function showProductArr(arr) {
-//    let productHtml = "";
-//    if(arr.length == 0) {
-//        productHtml = `<div class="no-result"><div class="no-result-i"><i class="fa-light fa-face-sad-cry"></i></div><div class="no-result-h">Không có sản phẩm để hiển thị</div></div>`;
-//    } else {
-//        arr.forEach(product => {
-//            let btnCtl = product.status == 1 ? 
-//            `<button class="btn-delete" onclick="deleteProduct(${product.id})"><i class="fa-regular fa-trash"></i></button>` :
-//            `<button class="btn-delete" onclick="changeStatusProduct(${product.id})"><i class="fa-regular fa-eye"></i></button>`;
-//            productHtml += `
-//            <div class="list">
-//                    <div class="list-left">
-//                    <img src="${product.img}" alt="">
-//                    <div class="list-info">
-//                        <h4>${product.title}</h4>
-//                        <p class="list-note">${product.desc}</p>
-//                        <span class="list-category">${product.category}</span>
-//                    </div>
-//                </div>
-//                <div class="list-right">
-//                    <div class="list-price">
-//                    <span class="list-current-price">${vnd(product.price)}</span>                   
-//                    </div>
-//                    <div class="list-control">
-//                    <div class="list-tool">
-//                        <button class="btn-edit" onclick="editProduct(${product.id})"><i class="fa-light fa-pen-to-square"></i></button>
-//                        ${btnCtl}
-//                    </div>                       
-//                </div>
-//                </div> 
-//            </div>`;
-//        });
-//    }
-//    document.getElementById("show-product").innerHTML = productHtml;
-//}
-
-//function showProduct() {
-//    let selectOp = document.getElementById('the-loai').value;
-//    let valeSearchInput = document.getElementById('form-search-product').value;
-//    let products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
-
-//    if(selectOp == "Tất cả") {
-//        result = products.filter((item) => item.status == 1);
-//    } else if(selectOp == "Đã xóa") {
-//        result = products.filter((item) => item.status == 0);
-//    } else {
-//        result = products.filter((item) => item.category == selectOp);
-//    }
-
-//    result = valeSearchInput == "" ? result : result.filter(item => {
-//        return item.title.toString().toUpperCase().includes(valeSearchInput.toString().toUpperCase());
-//    })
-
-//    displayList(result, perPage, currentPage);
-//    setupPagination(result, perPage, currentPage);
-//}
-
-//function cancelSearchProduct() {
-//    let products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")).filter(item => item.status == 1) : [];
-//    document.getElementById('the-loai').value = "Tất cả";
-//    document.getElementById('form-search-product').value = "";
-//    displayList(products, perPage, currentPage);
-//    setupPagination(products, perPage, currentPage);
-//}
-
-//window.onload = showProduct();
 
 function createId(arr) {
     let id = arr.length;
@@ -332,16 +226,16 @@ function setDefaultValue() {
 }
 
 // Open Popup Modal
-//let btnAddProduct = document.getElementById("btn-add-product");
-//btnAddProduct.addEventListener("click", () => {
-//    document.querySelectorAll(".add-product-e").forEach(item => {
-//        item.style.display = "block";
-//    })
-//    document.querySelectorAll(".edit-product-e").forEach(item => {
-//        item.style.display = "none";
-//    })
-//    document.querySelector(".add-product").classList.add("open");
-//});
+let btnAddProduct = document.getElementById("btn-add-product");
+btnAddProduct.addEventListener("click", () => {
+    document.querySelectorAll(".add-product-e").forEach(item => {
+        item.style.display = "block";
+    })
+    document.querySelectorAll(".edit-product-e").forEach(item => {
+        item.style.display = "none";
+    })
+    document.querySelector(".add-product").classList.add("open");
+});
 
 // Close Popup Modal
 let closePopup = document.querySelectorAll(".modal-close");
@@ -383,35 +277,6 @@ function formatDate(date) {
     if (mm < 10) mm = "0" + mm;
     return dd + "/" + mm + "/" + yyyy;
 }
-
-// Show order
-function showOrder(arr) {
-    let orderHtml = "";
-    if(arr.length == 0) {
-        orderHtml = `<td colspan="6">Không có dữ liệu</td>`
-    } else {
-        arr.forEach((item) => {
-            let status = item.trangthai == 0 ? `<span class="status-no-complete">Chưa xử lý</span>` : `<span class="status-complete">Đã xử lý</span>`;
-            let date = formatDate(item.thoigiandat);
-            orderHtml += `
-            <tr>
-            <td>${item.id}</td>
-            <td>${item.khachhang}</td>
-            <td>${date}</td>
-            <td>${vnd(item.tongtien)}</td>                               
-            <td>${status}</td>
-            <td class="control">
-            <button class="btn-detail" id="" onclick="detailOrder('${item.id}')"><i class="fa-regular fa-eye"></i> Chi tiết</button>
-            </td>
-            </tr>      
-            `;
-        });
-    }
-    document.getElementById("showOrder").innerHTML = orderHtml;
-}
-
-let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
-//window.onload = showOrder(orders);
 
 // Get Order Details
 function getOrderDetails(madon) {
@@ -548,150 +413,6 @@ function cancelSearchOrder(){
     document.getElementById("time-start").value = "";
     document.getElementById("time-end").value = "";
     showOrder(orders);
-}
-
-// Create Object Thong ke
-function createObj() {
-    let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
-    let products = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : []; 
-    let orderDetails = localStorage.getItem("orderDetails") ? JSON.parse(localStorage.getItem("orderDetails")) : []; 
-    let result = [];
-    orderDetails.forEach(item => {
-        // Lấy thông tin sản phẩm
-        let prod = products.find(product => {return product.id == item.id;});
-        let obj = new Object();
-        obj.id = item.id;
-        obj.madon = item.madon;
-        obj.price = item.price;
-        obj.quantity = item.soluong;
-        obj.category = prod.category;
-        obj.title = prod.title;
-        obj.img = prod.img;
-        obj.time = (orders.find(order => order.id == item.madon)).thoigiandat;
-        result.push(obj);
-    });
-    return result;
-}
-
-// Filter 
-function thongKe(mode) {
-    let categoryTk = document.getElementById("the-loai-tk").value;
-    let ct = document.getElementById("form-search-tk").value;
-    let timeStart = document.getElementById("time-start-tk").value;
-    let timeEnd = document.getElementById("time-end-tk").value;
-    if (timeEnd < timeStart && timeEnd != "" && timeStart != "") {
-        alert("Lựa chọn thời gian sai !");
-        return;
-    }
-    let arrDetail = createObj();
-    let result = categoryTk == "Tất cả" ? arrDetail : arrDetail.filter((item) => {
-        return item.category == categoryTk;
-    });
-
-    result = ct == "" ? result : result.filter((item) => {
-        return (item.title.toLowerCase().includes(ct.toLowerCase()));
-    });
-
-    if (timeStart != "" && timeEnd == "") {
-        result = result.filter((item) => {
-            return new Date(item.time) > new Date(timeStart).setHours(0, 0, 0);
-        });
-    } else if (timeStart == "" && timeEnd != "") {
-        result = result.filter((item) => {
-            return new Date(item.time) < new Date(timeEnd).setHours(23, 59, 59);
-        });
-    } else if (timeStart != "" && timeEnd != "") {
-        result = result.filter((item) => {
-            return (new Date(item.time) > new Date(timeStart).setHours(0, 0, 0) && new Date(item.time) < new Date(timeEnd).setHours(23, 59, 59)
-            );
-        });
-    }    
-    showThongKe(result,mode);
-}
-
-// Show số lượng sp, số lượng đơn bán, doanh thu
-function showOverview(arr){
-    document.getElementById("quantity-product").innerText = arr.length;
-    document.getElementById("quantity-order").innerText = arr.reduce((sum, cur) => (sum + parseInt(cur.quantity)),0);
-    document.getElementById("quantity-sale").innerText = vnd(arr.reduce((sum, cur) => (sum + parseInt(cur.doanhthu)),0));
-}
-
-function showThongKe(arr,mode) {
-    let orderHtml = "";
-    let mergeObj = mergeObjThongKe(arr);
-    showOverview(mergeObj);
-
-    switch (mode){
-        case 0:
-            mergeObj = mergeObjThongKe(createObj());
-            showOverview(mergeObj);
-            document.getElementById("the-loai-tk").value = "Tất cả";
-            document.getElementById("form-search-tk").value = "";
-            document.getElementById("time-start-tk").value = "";
-            document.getElementById("time-end-tk").value = "";
-            break;
-        case 1:
-            mergeObj.sort((a,b) => parseInt(a.quantity) - parseInt(b.quantity))
-            break;
-        case 2:
-            mergeObj.sort((a,b) => parseInt(b.quantity) - parseInt(a.quantity))
-            break;
-    }
-    for(let i = 0; i < mergeObj.length; i++) {
-        orderHtml += `
-        <tr>
-        <td>${i + 1}</td>
-        <td><div class="prod-img-title"><img class="prd-img-tbl" src="${mergeObj[i].img}" alt=""><p>${mergeObj[i].title}</p></div></td>
-        <td>${mergeObj[i].quantity}</td>
-        <td>${vnd(mergeObj[i].doanhthu)}</td>
-        <td><button class="btn-detail product-order-detail" data-id="${mergeObj[i].id}"><i class="fa-regular fa-eye"></i> Chi tiết</button></td>
-        </tr>      
-        `;
-    }
-    document.getElementById("showTk").innerHTML = orderHtml;
-    document.querySelectorAll(".product-order-detail").forEach(item => {
-        let idProduct = item.getAttribute("data-id");
-        item.addEventListener("click", () => {           
-            detailOrderProduct(arr,idProduct);
-        })
-    })
-}
-
-//showThongKe(createObj())
-
-function mergeObjThongKe(arr) {
-    let result = [];
-    arr.forEach(item => {
-        let check = result.find(i => i.id == item.id) // Không tìm thấy gì trả về undefined
-
-        if(check){
-            check.quantity = parseInt(check.quantity)  + parseInt(item.quantity);
-            check.doanhthu += parseInt(item.price) * parseInt(item.quantity);
-        } else {
-            const newItem = {...item}
-            newItem.doanhthu = newItem.price * newItem.quantity;
-            result.push(newItem);
-        }
-        
-    });
-    return result;
-}
-
-function detailOrderProduct(arr,id) {
-    let orderHtml = "";
-    arr.forEach(item => {
-        if(item.id == id) {
-            orderHtml += `<tr>
-            <td>${item.madon}</td>
-            <td>${item.quantity}</td>
-            <td>${vnd(item.price)}</td>
-            <td>${formatDate(item.time)}</td>
-            </tr>      
-            `;
-        }
-    });
-    document.getElementById("show-product-order-detail").innerHTML = orderHtml
-    document.querySelector(".modal.detail-order-product").classList.add("open")
 }
 
 // User
