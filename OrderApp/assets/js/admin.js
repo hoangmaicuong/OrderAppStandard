52c2556b82e9280a63efe40bc72b0ca4b4df00d1
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-08-24 16:58:29 +0700

    //

--- OrderApp/assets/js/admin.js
+++ OrderApp/assets/js/admin.js
@@ -225,8 +225,6 @@ function setDefaultValue() {
     document.getElementById("chon-mon").value = "Món chay";
 }
 
-
-
 // Close Popup Modal
 let closePopup = document.querySelectorAll(".modal-close");
 let modalPopup = document.querySelectorAll(".modal");
@@ -243,20 +241,6 @@ function uploadImage(el) {
     document.querySelector(".upload-image-preview").setAttribute("src", path);
 }
 
-// Đổi trạng thái đơn hàng
-function changeStatus(id, el) {
-    let orders = JSON.parse(localStorage.getItem("order"));
-    let order = orders.find((item) => {
-        return item.id == id;
-    });
-    order.trangthai = 1;
-    el.classList.remove("btn-chuaxuly");
-    el.classList.add("btn-daxuly");
-    el.innerHTML = "Đã xử lý";
-    localStorage.setItem("order", JSON.stringify(orders));
-    findOrder(orders);
-}
-
 // Format Date
 function formatDate(date) {
     let fm = new Date(date);
@@ -266,143 +250,6 @@ function formatDate(date) {
     if (dd < 10) dd = "0" + dd;
     if (mm < 10) mm = "0" + mm;
     return dd + "/" + mm + "/" + yyyy;
-}
-
-// Get Order Details
-function getOrderDetails(madon) {
-    let orderDetails = localStorage.getItem("orderDetails") ?
-        JSON.parse(localStorage.getItem("orderDetails")) : [];
-    let ctDon = [];
-    orderDetails.forEach((item) => {
-        if (item.madon == madon) {
-            ctDon.push(item);
-        }
-    });
-    return ctDon;
-}
-
-// Show Order Detail
-function detailOrder(id) {
-    document.querySelector(".modal.detail-order").classList.add("open");
-    let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
-    let products = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("products")) : [];
-    // Lấy hóa đơn 
-    let order = orders.find((item) => item.id == id);
-    // Lấy chi tiết hóa đơn
-    let ctDon = getOrderDetails(id);
-    let spHtml = `<div class="modal-detail-left"><div class="order-item-group">`;
-
-    ctDon.forEach((item) => {
-        let detaiSP = products.find(product => product.id == item.id);
-        spHtml += `<div class="order-product">
-            <div class="order-product-left">
-                <img src="${detaiSP.img}" alt="">
-                <div class="order-product-info">
-                    <h4>${detaiSP.title}</h4>
-                    <p class="order-product-note"><i class="fa-light fa-pen"></i> ${item.note}</p>
-                    <p class="order-product-quantity">SL: ${item.soluong}<p>
-                </div>
-            </div>
-            <div class="order-product-right">
-                <div class="order-product-price">
-                    <span class="order-product-current-price">${vnd(item.price)}</span>
-                </div>                         
-            </div>
-        </div>`;
-    });
-    spHtml += `</div></div>`;
-    spHtml += `<div class="modal-detail-right">
-        <ul class="detail-order-group">
-            <li class="detail-order-item">
-                <span class="detail-order-item-left"><i class="fa-light fa-calendar-days"></i> Ngày đặt hàng</span>
-                <span class="detail-order-item-right">${formatDate(order.thoigiandat)}</span>
-            </li>
-            <li class="detail-order-item">
-                <span class="detail-order-item-left"><i class="fa-light fa-truck"></i> Hình thức giao</span>
-                <span class="detail-order-item-right">${order.hinhthucgiao}</span>
-            </li>
-            <li class="detail-order-item">
-            <span class="detail-order-item-left"><i class="fa-thin fa-person"></i> Người nhận</span>
-            <span class="detail-order-item-right">${order.tenguoinhan}</span>
-            </li>
-            <li class="detail-order-item">
-            <span class="detail-order-item-left"><i class="fa-light fa-phone"></i> Số điện thoại</span>
-            <span class="detail-order-item-right">${order.sdtnhan}</span>
-            </li>
-            <li class="detail-order-item tb">
-                <span class="detail-order-item-left"><i class="fa-light fa-clock"></i> Thời gian giao</span>
-                <p class="detail-order-item-b">${(order.thoigiangiao == "" ? "" : (order.thoigiangiao + " - ")) + formatDate(order.ngaygiaohang)}</p>
-            </li>
-            <li class="detail-order-item tb">
-                <span class="detail-order-item-t"><i class="fa-light fa-location-dot"></i> Địa chỉ nhận</span>
-                <p class="detail-order-item-b">${order.diachinhan}</p>
-            </li>
-            <li class="detail-order-item tb">
-                <span class="detail-order-item-t"><i class="fa-light fa-note-sticky"></i> Ghi chú</span>
-                <p class="detail-order-item-b">${order.ghichu}</p>
-            </li>
-        </ul>
-    </div>`;
-    document.querySelector(".modal-detail-order").innerHTML = spHtml;
-
-    let classDetailBtn = order.trangthai == 0 ? "btn-chuaxuly" : "btn-daxuly";
-    let textDetailBtn = order.trangthai == 0 ? "Chưa xử lý" : "Đã xử lý";
-    document.querySelector(
-        ".modal-detail-bottom"
-    ).innerHTML = `<div class="modal-detail-bottom-left">
-        <div class="price-total">
-            <span class="thanhtien">Thành tiền</span>
-            <span class="price">${vnd(order.tongtien)}</span>
-        </div>
-    </div>
-    <div class="modal-detail-bottom-right">
-        <button class="modal-detail-btn ${classDetailBtn}" onclick="changeStatus('${order.id}',this)">${textDetailBtn}</button>
-    </div>`;
-}
-
-// Find Order
-function findOrder() {
-    let tinhTrang = parseInt(document.getElementById("tinh-trang").value);
-    let ct = document.getElementById("form-search-order").value;
-    let timeStart = document.getElementById("time-start").value;
-    let timeEnd = document.getElementById("time-end").value;
-    
-    if (timeEnd < timeStart && timeEnd != "" && timeStart != "") {
-        alert("Lựa chọn thời gian sai !");
-        return;
-    }
-    let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
-    let result = tinhTrang == 2 ? orders : orders.filter((item) => {
-        return item.trangthai == tinhTrang;
-    });
-    result = ct == "" ? result : result.filter((item) => {
-        return (item.khachhang.toLowerCase().includes(ct.toLowerCase()) || item.id.toString().toLowerCase().includes(ct.toLowerCase()));
-    });
-
-    if (timeStart != "" && timeEnd == "") {
-        result = result.filter((item) => {
-            return new Date(item.thoigiandat) >= new Date(timeStart).setHours(0, 0, 0);
-        });
-    } else if (timeStart == "" && timeEnd != "") {
-        result = result.filter((item) => {
-            return new Date(item.thoigiandat) <= new Date(timeEnd).setHours(23, 59, 59);
-        });
-    } else if (timeStart != "" && timeEnd != "") {
-        result = result.filter((item) => {
-            return (new Date(item.thoigiandat) >= new Date(timeStart).setHours(0, 0, 0) && new Date(item.thoigiandat) <= new Date(timeEnd).setHours(23, 59, 59)
-            );
-        });
-    }
-    showOrder(result);
-}
-
-function cancelSearchOrder(){
-    let orders = localStorage.getItem("order") ? JSON.parse(localStorage.getItem("order")) : [];
-    document.getElementById("tinh-trang").value = 2;
-    document.getElementById("form-search-order").value = "";
-    document.getElementById("time-start").value = "";
-    document.getElementById("time-end").value = "";
-    showOrder(orders);
 }
 
 // User
