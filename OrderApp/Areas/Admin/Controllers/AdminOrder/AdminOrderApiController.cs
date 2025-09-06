2125e81c01494d8ee2d810057965accc1efc3eae
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-09-06 11:38:25 +0700

    //

---
+++ OrderApp/Areas/Admin/Controllers/AdminOrder/AdminOrderApiController.cs
@@ -0,0 +1,23 @@
+﻿using OrderApp.Areas.Admin.Controllers.AdminTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OrderApp.Areas.Admin.Controllers.AdminOrder
+{
+    [Authorize]
+    [RoutePrefix("api/admin/order")]
+    public class AdminOrderApiController : ApiController
+    {
+        private AdminOrderService services = new AdminOrderService();
+        //[HttpGet]
+        //[Route("get-all")]
+        //public IHttpActionResult GetAll()
+        //{
+        //    return Ok(services.GetAll());
+        //}
+    }
+}
---
+++ OrderApp/Areas/Admin/Controllers/AdminOrder/AdminOrderService.cs
@@ -0,0 +1,45 @@
+﻿using OrderApp.DataFactory;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace OrderApp.Areas.Admin.Controllers.AdminOrder
+{
+    public class AdminOrderService
+    {
+        private OrderAppEntities db = new OrderAppEntities();
+        private DapperContext dapperContext = new DapperContext();
+        string imagePath = ConfigurationManager.AppSettings["ProductImageUploadPath"];
+        public DataSet GetAll()
+        {
+            using (var connec = dapperContext.CreateConnection())
+            {
+                connec.Open();
+                try
+                {
+                    string proce = "AdminTableModuleGetAll";
+                    using (var cmd = new SqlCommand(proce, connec))
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        using (var adapter = new SqlDataAdapter(cmd))
+                        {
+                            var dataSet = new DataSet();
+                            adapter.Fill(dataSet);
+                            dataSet.Tables[0].TableName = "tables";
+                            return dataSet;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                    throw;
+                }
+            }
+        }
+    }
+}
