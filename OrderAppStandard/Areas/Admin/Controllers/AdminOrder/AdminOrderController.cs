7ac1164afb7cfe927c13c44fb419b44486169ff8
Hoang Mai Cuong <dev.maicuong@gmail.com>
2025-09-25 21:19:31 +0700

    //

--- OrderAppStandard/Areas/Admin/Controllers/AdminOrder/AdminOrderController.cs
+++ OrderAppStandard/Areas/Admin/Controllers/AdminOrder/AdminOrderController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using OrderApp.DataFactory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +11,13 @@ namespace OrderApp.Areas.Admin.Controllers.AdminOrder
     [Authorize]
     public class AdminOrderController : Controller
     {
+        private OrderAppEntities db = new OrderAppEntities();
         // GET: Admin/AdminOrder
         public ActionResult Index()
         {
+            string userId = User.Identity.GetUserId();
+            var userExtension = db.UserExtension.FirstOrDefault(x => x.AspNetUserId == userId);
+            ViewBag.CompanySlug = userExtension?.Company.Slug;
             return View();
         }
     }
--- OrderAppStandard/Areas/Admin/Controllers/AdminTable/AdminTableController.cs
+++ OrderAppStandard/Areas/Admin/Controllers/AdminTable/AdminTableController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using OrderApp.DataFactory;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,8 +12,12 @@ namespace OrderApp.Areas.Admin.Controllers.AdminTable
     public class AdminTableController : Controller
     {
         // GET: Admin/AdminTable
+        private OrderAppEntities db = new OrderAppEntities();
         public ActionResult Index()
         {
+            string userId = User.Identity.GetUserId();
+            var userExtension = db.UserExtension.FirstOrDefault(x => x.AspNetUserId == userId);
+            ViewBag.CompanySlug = userExtension?.Company.Slug;
             return View();
         }
     }
