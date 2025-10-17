5611bb9302d266cbf5e21ca0e171cfd36f2d9061
Mai Cuong Hoang <sup.maicuong@outlook.com>
2025-10-17 13:17:23 +0700

    //

---
+++ OrderAppStandard/Areas/Admin/Controllers/AdminCompany/AdminCompanyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrderApp.Areas.Admin.Controllers.AdminCompany
+{
+    public class AdminCompanyController : Controller
+    {
+        // GET: Admin/AdminCompany
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
